d57b0cfc98e2366a2cb5ef5b97b50bba2ee87ad7
Brian Stark <hello@brianstark.de>
2022-02-13 21:53:57 +0100

    adjust reyche page

--- pages/nachbarn.vue
+++ pages/nachbarn.vue
@@ -19,7 +19,10 @@
         <div class="container nachbarn is-fullhd px-4">
           <a :href="reych.url" target="_blank" class="reych" v-for="reych of reyche" :key="reyche.nummer">
             <img :src="reych.logo" />
-            {{reych.name}}
+              <div class="reych__content">
+                {{reych.name}} {{reych.nummer}}
+                <div class="reych__city">{{reych.stadt}}</div>
+              </div>
           </a>
         </div>
       </section>
@@ -31,142 +34,142 @@ const alleNachbarn = [
   {
     name: 'Colonia Agrippina',
     logo: require('assets/images/reyche/colonia.jpg'),
-    stadt: '',
+    stadt: 'Köln',
     nummer: '(8)',
-    url: 'http://www.schlaraffia-colonia-agrippina.de"'
+    url: 'http://www.schlaraffia-colonia-agrippina.de'
   },
   {
     name: 'Elberfeldensis',
     logo: require('assets/images/reyche/elberfeldensis.jpg'),
-    stadt: '',
+    stadt: 'Wuppertal',
     nummer: '(34)',
-    url: 'http://www.elberfeldensis.de"'
+    url: 'http://www.elberfeldensis.de'
   },
-  {
-    name: 'Assindia',
-    logo: require('assets/images/reyche/assindia.jpg'),
-    stadt: '',
-    nummer: '(50)',
-    url: 'http://www.schlaraffia-assindia.org"'
-  },
+  // {
+  //   name: 'Assindia',
+  //   logo: require('assets/images/reyche/assindia.jpg'),
+  //   stadt: '',
+  //   nummer: '(50)',
+  //   url: 'http://www.schlaraffia-assindia.org'
+  // },
   {
     name: 'Aquisgranum',
     logo: require('assets/images/reyche/aquisgranum.jpg'),
-    stadt: '',
+    stadt: 'Aachen',
     nummer: '(94)',
-    url: 'http://www.aquisgranum.de"'
+    url: 'http://www.aquisgranum.de'
   },
   {
     name: 'Bochumensis',
     logo: require('assets/images/reyche/bochumensis.jpg'),
-    stadt: '',
+    stadt: 'Bochum',
     nummer: '(166)',
-    url: 'http://www.bochumensis.de"'
+    url: 'http://www.bochumensis.de'
   },
   {
     name: 'Truymannia',
     logo: require('assets/images/reyche/truymannia.jpg'),
-    stadt: '',
+    stadt: 'Dortmund',
     nummer: '(177)',
-    url: 'http://www.truymannia.de"'
+    url: 'http://www.truymannia.de'
   },
   {
     name: 'Hagena',
     logo: require('assets/images/reyche/hagena.jpg'),
-    stadt: '',
+    stadt: 'Hagen',
     nummer: '(189)',
-    url: 'http://www.schlaraffia-hagena.de"'
+    url: 'http://www.schlaraffia-hagena.de'
   },
   {
     name: 'Crefeldensis',
     logo: require('assets/images/reyche/crefeldensis.jpg'),
-    stadt: '',
+    stadt: 'Krefeld',
     nummer: '(191)',
-    url: 'http://www.crefeldensis.de"'
+    url: 'http://www.crefeldensis.de'
   },
   {
     name: 'Castrum Bonnense',
     logo: require('assets/images/reyche/castrum_bonnense.jpg'),
-    stadt: '',
+    stadt: 'Bonn',
     nummer: '(193)',
-    url: 'http://www.castrum-bonnense.de"'
+    url: 'http://www.castrum-bonnense.de'
   },
   {
     name: 'Mimegarda',
     logo: require('assets/images/reyche/mimegarda.jpg'),
-    stadt: '',
+    stadt: 'Münster',
     nummer: '(209)',
-    url: 'http://www.mimegarda.de"'
+    url: 'http://www.mimegarda.de'
   },
   {
     name: 'Confluentia',
     logo: require('assets/images/reyche/confluentia.jpg'),
-    stadt: '',
+    stadt: 'Koblenz',
     nummer: '(225)',
-    url: 'http://www.schlaraffia-confluentia.de"'
+    url: 'http://www.schlaraffia-confluentia.de'
   },
   {
     name: 'Duisbargum',
     logo: require('assets/images/reyche/duisbargum.jpg'),
-    stadt: '',
+    stadt: 'Duisburg',
     nummer: '(242)',
-    url: 'http://www.schlaraffia-duisbargum.de"'
+    url: 'https://www.duisbargum.de/'
   },
   {
     name: 'Gladebachum Monachorum',
     logo: require('assets/images/reyche/gladebachum.jpg'),
-    stadt: '',
+    stadt: 'Mönchengladbach',
     nummer: '(252)',
-    url: 'http://www.gladmo252.de"'
+    url: 'http://www.gladmo252.de'
   },
   {
     name: 'Moellmia',
     logo: require('assets/images/reyche/moellmia.jpg'),
-    stadt: '',
+    stadt: 'Mülheim an der Ruhr',
     nummer: '(253)',
-    url: 'http://www.moellmia.de"'
+    url: 'http://www.moellmia.de'
   },
   {
     name: 'An der Sieg',
     logo: require('assets/images/reyche/an_der_sieg.jpg'),
-    stadt: '',
+    stadt: 'Siegen',
     nummer: '(323)',
-    url: 'http://www.andersieg.de"'
+    url: 'https://andersieg.com/'
   },
   {
     name: 'Castellum Novaesium',
     logo: require('assets/images/reyche/castellum_novaesium.jpg'),
-    stadt: '',
+    stadt: 'Neuss',
     nummer: '(353)',
-    url: 'http://www.castellum-novaesium.de"'
+    url: 'http://www.castellum-novaesium.de'
   },
   {
     name: 'Porta Arduennae',
     logo: require('assets/images/reyche/portaarduennae.jpg'),
-    stadt: '',
+    stadt: 'Düren',
     nummer: '(367)',
-    url: 'http://www.portaarduennae.de/"'
+    url: 'http://www.portaarduennae.de/'
   },
   {
     name: 'Im Vest',
     logo: require('assets/images/reyche/imvest.jpg'),
-    stadt: '',
+    stadt: 'Gelsenkirchen',
     nummer: '(373)',
-    url: 'http://www.imvest373.de"'
+    url: 'http://www.imvest373.de'
   },
   {
     name: 'Sugambria',
     logo: require('assets/images/reyche/sugambria.jpg'),
-    stadt: '',
+    stadt: 'Lohmar',
     nummer: '(398)',
-    url: 'http://www.schlaraffia-sugambria.de"'
+    url: 'http://www.schlaraffia-sugambria.de'
   },
   {
     name: 'Brucsella',
     logo: require('assets/images/reyche/bruscella.jpg'),
-    stadt: '',
+    stadt: 'Brüssel',
     nummer: '(418)',
-    url: 'http://www.brucsella.os-home.de"'
+    url: 'http://www.brucsella.os-home.de'
   },
 ];
 export default {
@@ -183,16 +186,43 @@ export default {
     grid-row-gap: 16px;
 }
 .reych {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
   text-align: center;
   margin: 0 auto;
   max-width: 300px;
+  width: 300px;
   border-radius: 16px;
   transition: all ease-in-out 320ms;
-  padding: 3rem 1rem;
+  padding: 3rem;
+  border: solid 1px rgb(0 0 0 / 10%);
+  color: #0B7CB8;
 
   &:hover {
-    box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
+    box-shadow: rgb(0 0 0 / 10%) 0px 20px 25px -5px, rgb(0 0 0 / 4%) 0px 10px 10px -5px;
     background: #ffffff;
+  }
+
+  img {
+    max-width: 100px;
+    width: 100px;
+  }
+
+  &__content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 1rem;
+    font-weight: bold;
+  }
+
+  &__city {
+    font-style: italic;
+    color: #777;
+    font-weight: 300;
+    margin-top: 0.5rem;
   }
 }
 .main {
